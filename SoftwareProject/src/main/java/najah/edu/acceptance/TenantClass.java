e9b405b7f73f6faa7b1d4cdf2009b639b55368fc
AreejSawalha <137282264+AreejSawalha@users.noreply.github.com>
2023-08-08 14:07:22 +0300

    Update TenantClass.java

--- SoftwareProject/src/main/java/najah/edu/acceptance/TenantClass.java
+++ SoftwareProject/src/main/java/najah/edu/acceptance/TenantClass.java
@@ -2,13 +2,9 @@ package najah.edu.acceptance;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
-import java.util.logging.Logger;
-import java.util.logging.Level;
 
-
-public class TenantClass {
-	 private static final Logger LOGGER = Logger.getLogger(TenantClass.class.getName());
+class TenantClass {
+	
 	 private static final List<TenantClass> Tenant= new ArrayList<TenantClass>();
 	
 	private static  String name;

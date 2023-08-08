042023387b879770cd587122a015bac2767de11e
AreejSawalha <137282264+AreejSawalha@users.noreply.github.com>
2023-08-08 13:59:12 +0300

    Add files via upload

---
+++ SoftwareProject/src/main/java/najah/edu/acceptance/TenantClass.java
@@ -0,0 +1,21 @@
+package najah.edu.acceptance;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+
+public class TenantClass {
+	 private static final Logger LOGGER = Logger.getLogger(TenantClass.class.getName());
+	 private static final List<TenantClass> Tenant= new ArrayList<TenantClass>();
+	
+	private static  String name;
+	private static  String phone;
+	private static String location;
+	
+
+
+
+}

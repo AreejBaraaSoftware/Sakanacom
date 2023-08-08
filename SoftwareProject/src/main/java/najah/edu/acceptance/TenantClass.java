b35cbf70c10e5aedc068c757a3ab81bfeb3194a1
AreejSawalha <137282264+AreejSawalha@users.noreply.github.com>
2023-08-08 15:03:20 +0300

    Add files via upload

---
+++ SoftwareProject/src/main/java/najah/edu/acceptance/TenantClass.java
@@ -0,0 +1,17 @@
+package najah.edu.acceptance;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TenantClass {
+	
+	 private static final List<TenantClass> Tenant= new ArrayList<TenantClass>();
+	
+	private static  String name;
+	private static  String phone;
+	private static String location;
+	
+
+
+
+}
